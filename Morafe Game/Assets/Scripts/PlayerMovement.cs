4f0ac1822fb4b123d9b33cdbcbb2361de066ca16
therealthori <2570507@students.wits.ac.za>
2025-07-09 15:13:57 +0200

    Importing Assets

---
+++ Morafe Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 2;
+    public float horizontalSpeed = 3;
+    public float rightLimit = 5.5f;
+    public float leftLimit = -5.5f;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed,Space.World);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > leftLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < rightLimit)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed * -1);
+            }
+        }
+    }
+}
