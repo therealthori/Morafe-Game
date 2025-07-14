d13a82ef0c709fb7eb6d6d1020f8699a6855c891
therealthori <2570507@students.wits.ac.za>
2025-07-14 12:50:54 +0200

    Collision + Other Animation Done

--- Morafe Game/Assets/Scripts/CollisionDetect.cs
+++ Morafe Game/Assets/Scripts/CollisionDetect.cs
@@ -6,10 +6,22 @@ public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject playerAnim;
-
+    [SerializeField] private AudioSource collisionFX;
+    [SerializeField] private GameObject mainCam;
+    [SerializeField] private GameObject fadeOut;
+    
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(CollisionEnd());
+    }
+
+    IEnumerator CollisionEnd()
+    {
+        collisionFX.Play();
         player.GetComponent<PlayerMovement>().enabled = false;
-        playerAnim.GetComponent<Animator>().Play("Stumble Backwards");
+        playerAnim.GetComponent<Animator>().Play("Stuble Backwards");
+        mainCam.GetComponent<Animator>().Play("CollisionCam");
+        yield return new WaitForSeconds(3);
+        fadeOut.SetActive(true);
     }
 }
